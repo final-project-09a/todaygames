8bde14c36ad2b7075488250c4eea6851cd9197dc
dkswn <https://github.com/dkswn>
2024-01-08 18:08:16 +0900

    커뮤니티 영역 라우터 기본세팅

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,10 +3,12 @@ import Home from '../pages/home';
 import Detail from '../pages/detail';
 import { BoardPg } from '../pages/board/BoardPg';
 import { ViewDetail } from '../pages/board/ViewDetail';
+import { Register } from '../pages/board/Register';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<Register />} />
         <Route path="/board" element={<BoardPg />} />
         <Route path="/viewdetail" element={<ViewDetail />} />
         <Route path="/" element={<Home />} />
