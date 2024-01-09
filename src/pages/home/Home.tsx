e1946ec6239df7fd2b51afbf943d5ad2b3b6e231
Kwon Bora <sweetpt88@gmail.com>
2024-01-09 12:13:35 +0900

    update: STEAM API 프록시 설정 및 데이터 불러오기 (#2)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { getGames } from 'api/games';
 import React from 'react';
 
 const Home = () => {
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['games'],
+    queryFn: getGames
+  });
+
+  if (isLoading) {
+    return <p>로딩중입니다...</p>;
+  }
+  if (isError) {
+    return <p>알 수 없는 오류가 발생했습니다...</p>;
+  }
+
+  console.log(data);
+
   return <div>Home</div>;
 };
 
