814cf75ae29ecc19aa4238363a2e4725ac5adaf1
Kwon Bora <sweetpt88@gmail.com>
2024-01-11 22:05:42 +0900

    update: glitch 서버 세팅 및 데이터 불러오기 확인 (#2)

--- src/api/games.ts
+++ src/api/games.ts
@@ -1,26 +1,34 @@
 import axios from 'axios';
 
 const getGames = async () => {
-  // const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com';
-  // const steamApiUrl = 'https://api.steampowered.com/ISteamApps/GetAppList/v2';
-  // const PROXY_ID = process.env.REACT_APP_PROXY_ID;
-
   try {
-    const { data } = await axios.get(`/ISteamApps/GetAppList/v2`);
-    console.log(data.applist.apps);
-    return data.applist.apps;
+    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/applist`);
+    if (response.status === 200) {
+      const applist = response.data.applist.apps;
+      console.log('Data from server:', applist);
+      return applist;
+    } else {
+      console.error('응답에러: ', response.status);
+    }
   } catch (error) {
-    console.error('fetch 에러 : ', error);
+    console.error('fetch 에러: ', error);
     throw error;
   }
-  //   try {
-  //     // const response = await axios.get(`${process.env.REACT_APP_STEAM_API_URL}`);
-  //     const { data } = await axios.get('/ISteamApps/GetAppList/v2');
-  //     console.log(data);
-  //     return data;
-  //   } catch (error) {
-  //     console.error('fetch 에러: ', error);
-  //   }
 };
+
+// const gameDetails = async (appid: string) => {
+//   try {
+//     const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/appdetail/${appid}`);
+//     if (response.status === 200) {
+//       console.log('Data from server:', response.data);
+//       return response.data;
+//     } else {
+//       console.error('응답에러: ', response.status);
+//     }
+//   } catch (error) {
+//     console.error('fetch 에러: ', error);
+//     throw error;
+//   }
+// };
 
 export { getGames };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -15,33 +15,7 @@ const Home = () => {
 
   const [gameDetails, setGameDetails] = useState<GameDetails | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!isLoading && !isError && data && data.length > 0) {
-        const appid = data[100].appid;
-        // const steamApiUrl = 'https://store.steampowered.com/api/appdetails?appids=${appid}&l=korean';
-        try {
-          const config: AxiosRequestConfig = {
-            headers: { 'Content-Type': 'application/xml' }
-          };
-          const response = await axios.get(`/api/appdetails?appids=${appid}&l=korean`, config);
-          console.log(response);
-
-          // 데이터 처리
-          const responseData: GameDetails = response.data[appid]?.data;
-          if (responseData) {
-            setGameDetails(responseData);
-          } else {
-            console.error('데이터 결과 없음: ', response);
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-
-    fetchData();
-  }, [isLoading, isError, data]);
+  console.log(data);
 
   if (isLoading) {
     return <p>로딩중입니다...</p>;
@@ -50,7 +24,7 @@ const Home = () => {
     return <p>알 수 없는 오류가 발생했습니다...</p>;
   }
 
-  return <div>{gameDetails ? <h1>{gameDetails.name}</h1> : <p>게임 정보를 불러오는 중입니다...</p>} </div>;
+  return <div>게임리스트</div>;
 };
 
 export default Home;
