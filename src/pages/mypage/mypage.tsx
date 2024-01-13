36d72a1d1d27c251fa4b1e0b293d03ee03dece26
pompomko <sdw12334@gmail.com>
2024-01-14 06:39:50 +0900

    update 유저 프로필

--- src/pages/mypage/mypage.tsx
+++ src/pages/mypage/mypage.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 import { supabasedata } from 'shared/supabase';
-import { StUserinfoBOx } from './styles';
+import { StUserinfoBOx, Avatar, Username, UserDetails, UserDetail, UserWrapper } from './styles';
+import userimg from 'assets/img/userimg.png';
 
 interface User {
   email: string;
-  avatar_url: string | null;
+  avatar_url: any;
   username: string | null;
   admin: boolean | null;
   id: string;
+  Profile: string | null;
 }
 const MyPage = () => {
   const [userList, setUserList] = useState<User[]>([]);
@@ -51,25 +53,32 @@ const MyPage = () => {
 
     fetchUserInfo();
   }, []);
-  console.log(useruid);
-  console.log(userList);
+  // console.log(useruid);
+  // console.log(userList);
 
   return (
     <div>
-      <h2>My Page</h2>
-
-      <p>일단 모든 유저의 회원정보를 불러옴 이제 fillter로 현재 로그인한 회원정보만 표시할예정</p>
-
       <div>
-        {userList.map((user: User, index: number) => (
-          <StUserinfoBOx key={index}>
-            <p>Email: {user.email}</p>
-            <p>Avatar URL: {user.avatar_url}</p>
-            <p>Username: {user.username}</p>
-            <p>Admin: {user.admin ? 'Yes' : 'No'}</p>
-            <p>ID: {user.id}</p>
-          </StUserinfoBOx>
-        ))}
+        {userList
+          .filter((user) => user.id === useruid)
+          .map((user: User, index: number) => (
+            <>
+              <StUserinfoBOx key={index}>
+                {/* <Avatar src={user.avatar_url} alt="User Avatar" /> */}
+                {/* 아바타 부분은 예시를 위해 assets 폴더에 기본이미지 추가하였습니다 */}
+                {/* userinfo테이블에 url을 담는 형식이나 dkslaus수파베이스 스토리지도 사용해야할듯싶네요 */}
+                <Avatar src={userimg} alt="User Avatar" />
+
+                <UserWrapper>
+                  <Username>{user.username}</Username>
+                  {user.Profile}
+                </UserWrapper>
+              </StUserinfoBOx>
+              <UserDetail>
+                <strong>Email:</strong> {user.email}
+              </UserDetail>
+            </>
+          ))}
       </div>
     </div>
   );
