58c86647dd8274be7f1873567e80c5ad22feb72c
Kwon Bora <sweetpt88@gmail.com>
2024-01-18 22:45:57 +0900

    update: 로그인, 로그아웃 시 커스텀 모달 (#46)

---
+++ src/common/SuccessModal.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import success from 'assets/icons/success.svg';
+
+interface SuccessModalProps {
+  isOpen: boolean;
+  children: JSX.Element;
+}
+
+const SuccessModal = ({ isOpen, children }: SuccessModalProps) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <StModalWrapper>
+      <StSuccessIcon />
+      {children}
+    </StModalWrapper>
+  );
+};
+
+export default SuccessModal;
+
+const StModalWrapper = styled.div`
+  position: fixed;
+  top: 130px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 250px;
+  height: 50px;
+  border-radius: 10px;
+  border: 1px solid ${(props) => props.theme.color.white};
+  background: ${(props) => props.theme.color.black};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  user-select: none;
+`;
+
+const StSuccessIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  background: url(${success}) no-repeat center center;
+`;
--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { supabase } from 'shared/supabase';
 import { StNavContainer, StNavWrapper, StLogoWrapper, StMenuWrapper, StLogIn } from './styles';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/config/configStore';
-import Search from './Search';
 import styled from 'styled-components';
 import accountIcon from '../../assets/icons/accountIcon.svg';
+import GameSearch from './GameSearch';
+import SuccessModal from 'common/SuccessModal';
 
 const NavBar: React.FC = () => {
   const navigate = useNavigate();
 
+  // 모달 상태 관리
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+
   const user = useSelector((state: RootState) => state.userSlice.userInfo);
+
+  // 로그인 시 success 모달 오픈
+  useEffect(() => {
+    if (user) {
+      setModalContent('로그인에 성공하였습니다.');
+      setModalOpen(true);
+    }
+  }, [user]);
+
+  // success 모달 3초 뒤 사라지게
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setModalOpen(false);
+    }, 3000);
+
+    // Clean up
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isModalOpen, user]);
 
   const handleLogout = async () => {
     try {
       await supabase.auth.signOut();
-      alert('로그아웃 되었습니다.');
+      setModalContent('로그아웃 되었습니다.');
+      setModalOpen(true);
       navigate('/');
     } catch (error) {
       console.error(error);
-      alert('로그아웃 중에 오류가 발생했습니다.');
+      setModalContent('로그아웃에 실패하였습니다.');
+      setModalOpen(true);
     }
   };
 
@@ -38,7 +65,7 @@ const NavBar: React.FC = () => {
           <Link to={'/board'}>
             <h2>커뮤니티</h2>
           </Link>
-          <Search />
+          <GameSearch />
           <StLogIn>
             {user ? (
               <>
@@ -48,11 +75,15 @@ const NavBar: React.FC = () => {
                     <h2>마이페이지</h2>
                   </StMyPageLink>
                 </Link>
-
                 <h2 onClick={handleLogout}>로그아웃</h2>
               </>
             ) : (
               <Link to="/login">로그인</Link>
+            )}
+            {isModalOpen && (
+              <SuccessModal isOpen={isModalOpen}>
+                <p>{modalContent}</p>
+              </SuccessModal>
             )}
           </StLogIn>
         </StMenuWrapper>
