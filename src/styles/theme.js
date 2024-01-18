318de1e980108ea12ca8ce09c464606e1827c534
Dello96 <enadios03@gmail.com>
2024-01-18 10:59:55 +0900

    fix: theme파일 수정

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,9 +7,10 @@ const color = {
   black: '#191919', //프로젝트 들어가는 모든 검정색
   white: '#ffffff', // 프로젝트 들어가는 모든 하얀색
   primary: '#2D4FA6', // 버튼색상
-  secondary: '#2E2F3F', // 카테고리 커버 색상
+  secondary: '#2E2F3F', // 장르 태그 플래그 색상
   inputcolor: '#3A3A3A',
-  gray: '#666', // 카테고리 커버 색상
+  gray: '#292929',
+  lightgray: '#363636',
   postback: '#232323'
 };
 
