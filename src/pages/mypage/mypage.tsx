a90f3f0082afb96992e790a44910daba7ddfa3b8
Kwon Bora <sweetpt88@gmail.com>
2024-01-19 21:15:59 +0900

    style: 마이페이지 UI 수정 (#53)

--- src/pages/mypage/mypage.tsx
+++ src/pages/mypage/mypage.tsx
@@ -3,30 +3,29 @@ import { useState } from 'react';
 import { RootState } from 'redux/config/configStore';
 import {
   StUserinfoBOx,
-  Avatar,
-  Username,
-  UserWrapper,
-  ProfileBox,
-  ProfileTitle,
-  ProfileInput,
-  Textarea,
-  CharacterCount,
-  StUserinfoBOxTop,
+  StCount,
   ManageButton,
-  StBackground,
-  StLabel,
-  InputGroup
+  InputGroup,
+  StUserInfoContainer,
+  StMypageContainer,
+  StInput
 } from './styles';
 import userimg from 'assets/img/userimg.png';
+import MypageNav from 'components/mypage/MypageNav';
 
 const MyPage = () => {
   const [profile, setProfile] = useState('');
 
   // userSlice의 상태관리를 위해 상태 가져오기
   const user = useSelector((state: RootState) => state.userSlice.userInfo);
+  const [nickName, setNickName] = useState(user?.username ? user.username : '');
 
   const handleProfileChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setProfile(e.target.value);
+  };
+
+  const handleNickNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickName(e.target.value);
   };
 
   if (!user) {
@@ -36,50 +35,53 @@ const MyPage = () => {
   console.log(user);
 
   return (
-    <div>
+    <>
       {user && (
-        <StBackground>
-          <StUserinfoBOxTop>
-            {/* <Avatar src={user.avatar_url} alt="User Avatar" /> */}
-            {/* 아바타 부분은 예시를 위해 assets 폴더에 기본이미지 추가하였습니다 */}
-            {/* userinfo테이블에 url을 담는 형식이나 dkslaus수파베이스 스토리지도 사용해야할듯싶네요 */}
-            <Avatar src={userimg} alt="User Avatar" />
+        <StMypageContainer>
+          {/* <Avatar src={user.avatar_url} alt="User Avatar" /> */}
+          {/* 아바타 부분은 예시를 위해 assets 폴더에 기본이미지 추가하였습니다 */}
+          {/* userinfo테이블에 url을 담는 형식이나 dkslaus수파베이스 스토리지도 사용해야할듯싶네요 */}
+          <MypageNav user={user} />
+          <StUserInfoContainer>
+            <StUserinfoBOx>
+              <h2>프로필 정보</h2>
+              <label>닉네임</label>
+              <StInput
+                type="text"
+                value={nickName}
+                onChange={handleNickNameChange}
+                maxLength={30}
+                minLength={1}
+                placeholder={user.username || '닉네임 (1 ~ 30자)'}
+              />
+              <StCount>{nickName.length} / 30</StCount>
 
-            <UserWrapper>
-              <Username>{user.username}</Username>
-              {user.profile}
-            </UserWrapper>
-          </StUserinfoBOxTop>
-
-          <StUserinfoBOx>
-            <ProfileTitle>프로필 정보</ProfileTitle>
-            <ProfileBox>
-              <ProfileTitle>닉네임</ProfileTitle>
-              <ProfileInput type="text" maxLength={30} placeholder={user.username || '닉네임 수정'} />
-              <ProfileTitle>프로필 소개</ProfileTitle>
-              <Textarea value={profile} onChange={handleProfileChange} placeholder="프로필 소개를 입력하세요." />
-              <CharacterCount>{profile.length} / 200</CharacterCount>
-            </ProfileBox>
-          </StUserinfoBOx>
-          <StUserinfoBOx>
-            <ProfileTitle>계정 관리</ProfileTitle>
-            <ProfileBox>
-              {' '}
-              <StLabel htmlFor="email">이메일</StLabel>
+              <label>프로필 소개</label>
+              <textarea
+                value={profile}
+                maxLength={200}
+                onChange={handleProfileChange}
+                placeholder="프로필 소개 (0 ~ 200자)"
+              />
+              <StCount>{profile.length} / 200</StCount>
+            </StUserinfoBOx>
+            <StUserinfoBOx>
+              <h2>계정 관리</h2>
+              <label htmlFor="email">이메일</label>
               <InputGroup>
-                <ProfileInput id="email" type="email" placeholder={user.email} />
-                <ManageButton>관리</ManageButton>
+                <input id="email" type="email" placeholder={user.email} />
+                {/* <ManageButton>관리</ManageButton> */}
               </InputGroup>
-              <StLabel htmlFor="password">비밀번호 변경</StLabel>
+              <label htmlFor="password">비밀번호 변경</label>
               <InputGroup>
-                <ProfileInput id="password" type="password" />
-                <ManageButton>전송</ManageButton>
+                <input id="password" type="password" />
+                {/* <ManageButton>전송</ManageButton> */}
               </InputGroup>
-            </ProfileBox>
-          </StUserinfoBOx>
-        </StBackground>
+            </StUserinfoBOx>
+          </StUserInfoContainer>
+        </StMypageContainer>
       )}
-    </div>
+    </>
   );
 };
 
--- src/pages/mypage/styles.ts
+++ src/pages/mypage/styles.ts
@@ -1,5 +1,89 @@
 import styled from 'styled-components';
 
+export const StMypageContainer = styled.div`
+  margin: 50px auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const StUserInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+
+export const StUserinfoBOx = styled.div`
+  width: 1100px;
+  border-radius: 10px;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.color.gray};
+  margin-bottom: 30px;
+  & h2 {
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+  & label {
+    font-size: 14px;
+    font-weight: 400;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+  & input {
+    position: relative;
+    width: 100%;
+    padding: 18px;
+    border-radius: 10px;
+    height: 48px;
+    background: ${(props) => props.theme.color.inputcolor};
+    color: ${(props) => props.theme.color.white};
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
+  & textarea {
+    width: 100%;
+    height: 144px;
+    border-radius: 10px;
+    background: ${(props) => props.theme.color.inputcolor};
+    color: ${(props) => props.theme.color.white};
+    border: none;
+    padding: 18px;
+    resize: none;
+    line-height: 1.5;
+  }
+`;
+
+export const StInput = styled.input`
+  position: relative;
+  width: 100%;
+  padding: 18px;
+  border-radius: 10px;
+  height: 48px;
+  background: ${(props) => props.theme.color.inputcolor};
+  color: ${(props) => props.theme.color.white};
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const StCount = styled.div`
+  color: #999;
+  text-align: right;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 15px;
+  margin-top: 15px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
 export const StLabel = styled.label`
   color: ${(props) => props.theme.color.black};
 `;
@@ -8,53 +92,6 @@ export const InputGroup = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
-
-export const StBackground = styled.div`
-  background-color: #f7f9f9;
-`;
-export const StUserinfoBOx = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
-  background-color: ${(props) => props.theme.color.white};
-  padding: 20px;
-  margin: 20px auto;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-  width: 80%; // 박스 크기 조절
-  color: ${(props) => props.theme.color.black};
-`;
-
-// export const Sth1 = styled.h1`
-//   color: ${(props) => props.theme.color.black};
-// `;
-
-export const StUserinfoBOxTop = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
-  background-color: #f1f1f1;
-  padding: 20px;
-  margin: 20px auto;
-
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-  background-color: ${(props) => props.theme.color.black};
-  width: 100%;
-`;
-
-export const Avatar = styled.img`
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  margin-right: 20px;
-`;
-
-export const Username = styled.h3`
-  font-size: ${(props) => props.theme.fontSize.xxxl};
-  color: ${(props) => props.theme.color.white};
 `;
 
 export const UserDetails = styled.div`
@@ -72,46 +109,6 @@ export const UserDetail = styled.p`
   }
 `;
 
-export const UserWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-`;
-
-export const ProfileBox = styled.div`
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  margin: 20px;
-  padding: 20px;
-  /* border: 1px solid #ddd; */
-  border-radius: 10px;
-  /* width: calc(100% - 40px); */
-  width: 95%;
-  background-color: ${(props) => props.theme.color.white};
-`;
-
-export const ProfileTitle = styled.h2`
-  font-size: ${(props) => props.theme.fontSize.xxl};
-  color: ${(props) => props.theme.color.black};
-  margin-bottom: 10px;
-`;
-
-export const ProfileInput = styled.input`
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  margin-bottom: 20px;
-  height: 50px;
-
-  margin-right: 10px;
-  size: 95px;
-  &:focus {
-    outline: none;
-    border-color: ${(props) => props.theme.color.primary};
-  }
-`;
 export const Textarea = styled.textarea`
   width: 100%;
   height: 60px;
@@ -119,13 +116,6 @@ export const Textarea = styled.textarea`
   border-radius: 5px;
   border: 1px solid #ccc;
   resize: none; // 사용자가 텍스트 영역 크기를 조절하지 못하도록
-`;
-
-export const CharacterCount = styled.div`
-  text-align: right;
-  margin-top: 5px;
-  font-size: 0.875rem;
-  color: #666;
 `;
 
 export const ManageButton = styled.button`
