45fafab86af90582ef15aff5922c00e9222455ea
Kwon Bora <sweetpt88@gmail.com>
2024-01-26 01:46:55 +0900

    update: 찜 기능 구현 및 supabase 데이터 연결 (#4)

--- src/components/detailpage/GameTitle.tsx
+++ src/components/detailpage/GameTitle.tsx
@@ -1,9 +1,16 @@
 import CustomCarousel from 'common/CustomCarousel';
 import { DataContext } from 'pages/detail/Detail';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import nextIcon from 'assets/icons/nextIcon.svg';
 import prevIcon from 'assets/icons/prevIcon.svg';
+import Button from 'common/Button';
+import heartIcon from 'assets/icons/heartIcon.svg';
+import redHeartIcon from 'assets/icons/redHeartIcon.svg';
+import { supabase } from 'types/supabase';
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/config/configStore';
+import { GameType } from 'types/games';
 
 const settings = {
   infinite: true,
@@ -15,19 +22,81 @@ const settings = {
 };
 
 const GameTitle = () => {
-  const data = useContext(DataContext);
+  const data = useContext(DataContext) as GameType;
   const screenShots = data?.screenshots;
   const [currentCenter, setCurrentCenter] = useState(0);
+  const [isBookmarked, setIsBookmarked] = useState(false);
 
-  console.log(currentCenter);
+  const user = useSelector((state: RootState) => state.userSlice.userInfo);
+
+  useEffect(() => {
+    const checkBookmarked = async () => {
+      if (user?.id && data?.steam_appid) {
+        const { data: bookmark, error } = await supabase
+          .from('user_bookmarks')
+          .select('user_id, app_id')
+          .eq('user_id', user.id)
+          .eq('app_id', data.steam_appid);
+
+        if (error) {
+          console.error('Error checking bookmark:', error);
+          return;
+        }
+
+        setIsBookmarked(bookmark.length > 0);
+      }
+    };
+
+    checkBookmarked();
+  }, [user?.id, data?.appid]);
 
   const handleAfterChange = (currentSlide: number) => {
     setCurrentCenter(currentSlide);
   };
 
+  const handleGameSiteButtonClick = () => {
+    if (data?.website) {
+      window.open(data.website, '_blank');
+    }
+  };
+
+  const handleBookmarkClick = async (userId: string, appId: number) => {
+    if (user) {
+      try {
+        if (isBookmarked) {
+          await supabase.from('user_bookmarks').delete().eq('user_id', userId).eq('app_id', appId);
+        } else {
+          await supabase.from('user_bookmarks').upsert([
+            {
+              user_id: userId,
+              app_id: appId
+            }
+          ]);
+        }
+
+        setIsBookmarked(!isBookmarked);
+      } catch (error) {
+        console.error('Error handling bookmark:', error);
+      }
+    }
+  };
+
   return (
     <div>
-      <StTitle>{data?.name}</StTitle>
+      <StTitleWrpapper>
+        <StTitle>{data?.name}</StTitle>
+        <StButtonWrapper>
+          <Button type="button" size="medium" onClick={handleGameSiteButtonClick}>
+            공식사이트
+          </Button>
+          <StHeartIconWrapper
+            onClick={() => user?.id && handleBookmarkClick(user?.id, data.steam_appid)}
+            $isBookmarked={isBookmarked}
+          >
+            <img src={isBookmarked ? redHeartIcon : heartIcon} alt="찜하기" />
+          </StHeartIconWrapper>
+        </StButtonWrapper>
+      </StTitleWrpapper>
       <StMainImageWrapper>
         <img src={screenShots?.[currentCenter]?.path_thumbnail} alt={data?.name} />
       </StMainImageWrapper>
@@ -35,7 +104,7 @@ const GameTitle = () => {
         <CustomCarousel settings={{ ...settings, afterChange: handleAfterChange }}>
           {screenShots?.map((screenShot, index) => (
             <div key={index}>
-              <StImageWrapper isCenter={index === currentCenter}>
+              <StImageWrapper $isCenter={index === currentCenter}>
                 <img src={screenShot.path_thumbnail} />
               </StImageWrapper>
             </div>
@@ -47,6 +116,33 @@ const GameTitle = () => {
 };
 
 export default GameTitle;
+
+const StTitleWrpapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+const StButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const StHeartIconWrapper = styled.div<{ $isBookmarked: boolean }>`
+  width: 48px;
+  height: 48px;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.color.gray};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  & img {
+    color: ${(props) => (props.$isBookmarked ? 'red' : props.theme.color.white)};
+  }
+`;
 
 const StMainImageWrapper = styled.figure`
   width: 100%;
@@ -68,17 +164,16 @@ const StTitle = styled.h1`
   color: ${(props) => props.theme.color.white};
   font-size: 50px;
   font-weight: 700;
-  margin-bottom: 40px;
 `;
 
-const StImageWrapper = styled.figure<{ isCenter: boolean }>`
+const StImageWrapper = styled.figure<{ $isCenter: boolean }>`
   width: 220px;
   height: 160px;
   border-radius: 10px;
   display: flex;
   margin-left: 14px;
   overflow: hidden;
-  border: ${(props) => (props.isCenter ? '3px solid white' : 'none')};
+  border: ${(props) => (props.$isCenter ? '3px solid white' : 'none')};
   & img {
     width: 100%;
     height: 100%;
