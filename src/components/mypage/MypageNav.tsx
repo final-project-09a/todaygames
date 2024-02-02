4551236c4c772720407788e3ab6691859b986e36
pompomko <sdw12334@gmail.com>
2024-02-02 20:07:40 +0900

    update 프로필 이미지 업로드 기능 완료

--- src/components/mypage/MypageNav.tsx
+++ src/components/mypage/MypageNav.tsx
@@ -24,6 +24,7 @@ const MypageNav = ({ selectedCategory, onCategoryChange }: MypageProps) => {
 
   const handleFileChange = useCallback(
     async (e: React.ChangeEvent<HTMLInputElement>) => {
+      let uploadedImageUrls = '';
       if (e.target) {
         const selectedFile = e.target.files;
         if (selectedFile && selectedFile.length > 0) {
@@ -32,8 +33,17 @@ const MypageNav = ({ selectedCategory, onCategoryChange }: MypageProps) => {
           const safeUserName = user?.nickname?.replace(/\s+/g, '').replace(/[^a-zA-Z0-9]/g, '_');
           // 파일 이름을 안전한 형태로 변환
           const safeFileName = file.name.replace(/[^a-zA-Z0-9.\-_]/g, '_');
+          const today = new Date();
+          const formattedYear = today.getFullYear().toString().slice(-2);
+          const formattedMonth = (today.getMonth() + 1).toString().padStart(2, '0');
+          const formattedDate = today.getDate().toString().padStart(2, '0');
+          const formattedHour = today.getHours().toString().padStart(2, '0');
+          const formattedMinute = today.getMinutes().toString().padStart(2, '0');
+          const formattedSecond = today.getSeconds().toString().padStart(2, '0');
+          const formattedFull = `${formattedYear}Y ${formattedMonth}M ${formattedDate}D ${formattedHour}H ${formattedMinute}M ${formattedSecond}S`;
+
           // const filePath = `${safeUserName}/${safeFileName}`;
-          const filePath = `${user?.id}`;
+          const filePath = `${user?.id}${formattedFull}`;
 
           // 먼저 이미지를 업로드 시도합니다.
           const { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file);
@@ -50,8 +60,24 @@ const MypageNav = ({ selectedCategory, onCategoryChange }: MypageProps) => {
               return;
             }
           }
-
+          // 업로드에 성공한 경우, 이미지의 URL을 가져옵니다.
+          const { data: publicURL } = await supabase.storage.from('avatars').getPublicUrl(filePath);
+          uploadedImageUrls = publicURL.publicUrl;
           alert('이미지 업로드 성공');
+          console.log(uploadedImageUrls);
+
+          const { error: updateError } = await supabase
+            .from('userinfo')
+            .update({ avatar_url: uploadedImageUrls })
+            .eq('id', user?.id);
+
+          // 업데이트에 실패한 경우
+          if (updateError) {
+            console.error('Error updating avatar_url:', updateError.message);
+            alert('프로필 이미지 업데이트 실패');
+          } else {
+            alert('프로필 이미지 업데이트 성공');
+          }
         }
       }
     },
